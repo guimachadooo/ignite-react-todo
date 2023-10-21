e0652faaa2b64d5f33d3bcdeef9be593389733b8
Gui Machado <machado757@hotmail.com>
2023-10-21 19:26:38 -0300

    fix storage

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,15 @@ import styles from "./App.module.css";
 import "./global.css";
 
 function App() {
-
-  let localTasks = JSON.parse(localStorage.getItem('tasks') || []);
-  let [tasks, setTasks] = useState(localTasks ? localTasks : []);
+  let getStorage = localStorage.getItem('tasks') || localStorage.setItem('tasks', JSON.stringify([]));
+  let localTasks = JSON.parse(getStorage || []);
+  
+  let [tasks, setTasks] = useState(localTasks);
 
   useEffect(() => {
     localStorage.setItem('tasks', JSON.stringify(tasks));
   }, [tasks]);
-  
+
   return (
     <div className={styles.none}>
       <Header tasks={tasks} setTasks={setTasks} />
